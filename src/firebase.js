5ebad76aa99a7806e8da61627fa8ef9bad6281e0
JohnC-92 <chongxz@solab.me.ntu.edu.tw>
2021-03-10 08:45:30 +0800

    fixed firebase.js config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyqJ5r40NwfDwyuQkLIjLBbfMkhzVGZdg",
-  authDomain: "fir-5465c.firebaseapp.com",
-  databaseURL: "https://fir-5465c.firebaseio.com",
-  projectId: "fir-5465c",
-  storageBucket: "fir-5465c.appspot.com",
-  messagingSenderId: "817527769962",
-  appId: "1:817527769962:web:dec4c7b7d49c7493a3f313",
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
